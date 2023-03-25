992e123bab04ff4655c1c12d043d97ce3b478682
Bryce Morries <brycemorries@gmail.com>
2023-03-25 11:26:10 -0500

    added 6 albums to the collection and console logged the collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,5 +8,13 @@ function addToCollection(title, artists, yearPublished) {
         yearPublished: yearPublished
     };
     collection.push(album)
-    return album;
+    return console.log(album);
 }
+addToCollection('Demon Days', 'Gorrilaz', '2005');
+addToCollection('Man On The Moon', 'Kid Cudi', '2009');
+addToCollection('Mister Asylum', 'Highley Suspect', '2015');
+addToCollection('-!-', 'Dead Poet Society', '2021');
+addToCollection('USB', 'Fred Again..', '2022');
+addToCollection('KIDS SEE GHOSTS', 'Kid Cudi', '2009');
+
+console.log(collection);
