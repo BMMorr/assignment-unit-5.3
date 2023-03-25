a0cb39915fe886cbc4f73b89635b7816d685d9ce
Bryce Morries <brycemorries@gmail.com>
2023-03-25 11:13:08 -0500

    created a function with 3 parameters that pushes an object into an array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,12 @@
 console.log('***** Music Collection *****')
+
+collection = [];
+function addToCollection(title, artists, yearPublished) {
+    album = {
+        title: title,
+        artists: artists,
+        yearPublished: yearPublished
+    };
+    collection.push(album)
+    return album;
+}
